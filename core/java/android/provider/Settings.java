9c181e6798b4b6bdc47f17046c556a32da36bcb9
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-17 22:00:05 +0800

    SystemUI: Implement battery level text customisations
    3 options - hidden, show beside, or show inside.
    
    Forward port the battery text-related changes from CM 13.0:
    2e0a11bb0bbf22c32f6e15d97ccac10414ec7b44 Forward port battery icon options
    738f5a87a361c9c1027e3764072a37a50467fe6d SystemUI: Expose values in status bar expanded header
    9500061a111fed25bed3ad30173d8e35063f32d0 Themes: Expose hard code for themes in SystemUI
    
    Change-Id: Idec48a3536a3dd529f7dd6076bf33a20cd8020c1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3691,6 +3691,26 @@ public final class Settings {
         public static final String GESTURE_TWO_FINGER_SWIPE = "gesture_two_finger_swipe";
 
         /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 2: Display the battery as a circle
+         * 4: Hide the battery status information
+         * 6: Display the battery as plain text
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -3758,6 +3778,8 @@ public final class Settings {
             GESTURE_ONE_FINGER_SWIPE_RIGHT,
             GESTURE_ONE_FINGER_SWIPE_DOWN,
             GESTURE_ONE_FINGER_SWIPE_LEFT,
+            STATUS_BAR_BATTERY_STYLE,
+            STATUS_BAR_SHOW_BATTERY_PERCENT
         };
 
         /**
